f8bc35ebd22812d4f45763cdd46376702989aef6
rawaid <rawaidakhtar@gmail.com>
2015-04-29 20:10:51 -0400

    moduler error...

--- app.js
+++ app.js
@@ -1,13 +1,78 @@
-angular.module('playerModule',['ngAudio'])
+angular.module('playerModule',['ngRoute','ngAudio', 'ngFx', 'ngAnimate'])
 .controller("playerController",function($scope,ngAudio, $timeout){
-	
+
+	$scope.nowPlaying = {
+		currentTrack:'track1'
+	};
+	$scope.albArt = {
+		art: 'tame.jpg'
+	};
 	$scope.bgImg = {
 		background: 'tame.jpg'
-
 	};
-	console.log('current background img: ' + $scope.bgImg.background)
+
+	$scope.track1 = {
+
+	    "track": {
+	        "id" : 42,
+	        "url" : "sounds/mySound.mp3",
+	        "albumArtUrl" : "tame.jpg",
+	        "name" : "Let It Happen",
+	        "artists" : ["Tame Impala"],
+	        "album" : "Let It Happen",
+	    },
+	    "station": {
+	        "id": 42,
+	        "name": "rock",
+	        "tag0": {
+	            "id": 42,
+	            "name": "rock",
+	            "isActive": 1,
+	            "tracks": 17,
+	            "stations": 23
+	        },
+	        "tag1": {
+	            "id": 23,
+	            "name": "classic rock",
+	            "isActive": 1,
+	            "tracks": 7,
+	            "stations": 30
+	        }
+	    }
+	};
+
+	$scope.track2 = {
+
+	    "track": {
+	        "id" : 42,
+	        "url" : "sounds/air.mp3",
+	        "albumArtUrl" : "iu.jpg",
+	        "name" : "Air You Out",
+	        "artists" : ["Dave Fol"],
+	        "album" : "Ithaca Underground Presents: Vol 1",
+	    },
+	    "station": {
+	        "id": 42,
+	        "name": "rock",
+	        "tag0": {
+	            "id": 42,
+	            "name": "rock",
+	            "isActive": 1,
+	            "tracks": 17,
+	            "stations": 23
+	        },
+	        "tag1": {
+	            "id": 23,
+	            "name": "classic rock",
+	            "isActive": 1,
+	            "tracks": 7,
+	            "stations": 30
+	        }
+	    }
+	};
 	
-    $scope.audio = ngAudio.load("sounds/mySound.mp3"); // returns NgAudioObject
+	
+    $scope.audio = ngAudio.load($scope.track1.track.url); // returns NgAudioObject
     $scope.stopTrack = function(){
     	$scope.audio.stop();
     	$timeout($scope.trackChanged, .01);
@@ -15,19 +80,26 @@ angular.module('playerModule',['ngAudio'])
 
     };
     $scope.trackChanged = function(){
-    	$scope.audio = ngAudio.load("sounds/newSound.mp3");
-    	//$scope.bgImg.background = 'https://f1.bcbits.com/img/a0699927521_2.jpg';
-    	//console.log($scope.bgImg.background);
-    	console.log('audio loaded');
+    	$scope.audio = ngAudio.load($scope.track2.track.url);
+    	$scope.nowPlaying.currentTrack = 'track2';
     	$scope.audio.play();
 
     };
     $scope.bgChanged = function(){
-		console.log('background changed!');
-		$scope.bgImg.background = 'reader.jpg';
-		$timeout($scope.$apply(), 10000);
-		console.log('new bg img: ' + $scope.bgImg.background);
+		$scope.bgImg.background = $scope.track2.track.albumArtUrl;
+	};
+	$scope.albArtChanged = function(){
+		$scope.albArt.art = $scope.track2.track.albumArtUrl;
 	};
 
+	$scope.getCurrentTrack = function(){
+		if ($scope.nowPlaying.currentTrack == 'track1'){
+			return $scope.track1;
+		}
+		else if ($scope.nowPlaying.currentTrack == 'track2'){
+			return $scope.track2;
+		}
+	};
 
 });  
+
